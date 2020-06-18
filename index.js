853bb428c5afa8b006765b533e688c7afb56d3d9
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-18 02:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -22,3 +22,36 @@ function mapToDouble(src){
   }
   return arr;
 }
+function mapToSquare(src){
+  let arr = [];
+  for (let i = 0; i < src.length; i++){
+  arr.push (src[i] * src[i])
+  }
+  return arr;
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(sourceArray){
+  let total = []
+  for (let i = 0; i < sourceArray.length; i++){
+    if (!sourceArray[i]) return false;
+  }
+  return true;
+  
+  
+}
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
+
+
